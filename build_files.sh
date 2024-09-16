64fb37f559b786c946b8feb2315804e7231fd75c
Krishna Chaurasiya <krishnachaurasiya2907@gmail.com>
2024-09-16 16:00:36 +0530

    changes in buildfiles

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,10 @@
+# echo "BUILD START"
+# python3 -m pip install -r requirements.txt
+# python3 manage.py collectstatic --noinput --clear
+# echo "BUILD END"
+
 echo "BUILD START"
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput --clear
+apt-get update && apt-get install -y libsqlite3-dev  # Install SQLite dependency
+python3 -m pip install -r requirements.txt  # Install Python dependencies
+python3 manage.py collectstatic --noinput --clear  # Collect static files
 echo "BUILD END"
